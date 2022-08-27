9de45f68898b5fa6004c843db7b348da210fe3d6
Rita Vlas <ritavlas@yandex.ru>
2022-08-27 12:18:12 +0300

    обновление

--- parser/Program.cs
+++ parser/Program.cs
@@ -10,7 +10,7 @@
                 string? fileName = Console.ReadLine();
                 FileParser? parser = FileParser.GetParser(fileName);
                 if (parser == null)
-                    Console.WriteLine("Парсер для данного файла не был найден! Попробуйте снова!");
+                    Console.WriteLine("Парсер для данного файла не был найден! Попробуйте еще раз!");
                 else
                     parser.Parse();
                 Console.WriteLine();
